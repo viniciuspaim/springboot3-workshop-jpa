94e248e9c3e0439bfda1593bdeb22c4060e80a60
VINÃO <viniciuspaimdacosta@yahoo.com.br>
2024-06-28 11:13:45 -0300

    Comentado H2 Dialect "Depracated" Adicionado Get para listar todos os usuários Adicionado Get para listar usuários por id Criada nova Classe UserService

--- src/main/java/com/aulanjava/course/resources/UserResource.java
+++ src/main/java/com/aulanjava/course/resources/UserResource.java
@@ -1,17 +1,32 @@
 package com.aulanjava.course.resources;
 
 import com.aulanjava.course.entities.User;
+import com.aulanjava.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Vinicius", "vinicius.paim@proton.me", "9999999", "12345");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.finById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/aulanjava/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.aulanjava.course.services;
+
+import com.aulanjava.course.entities.User;
+import com.aulanjava.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User finById(Long id){
+       Optional<User> obj = repository.findById(id);
+       return obj.get();
+    }
+}
